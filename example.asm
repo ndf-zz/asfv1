ab86d46e651ab89afbb8750c67aef3d878cc959d
Nathan Fraser <ndf@metarace.com.au>
2019-07-20 01:07:41 +1000

    update to match readme

--- example.asm
+++ example.asm
@@ -3,7 +3,7 @@
         EQU     input   ADCL            ; read input from ADCL
         EQU     output  DACL            ; write output to DACL
         EQU     vol     REG0            ; use REG0 for volume
-        start:  skp     run,main        ; skip to main after first sample
+        start:  skp     RUN,main        ; skip to main after first sample
                 ldax    POT0            ; read from POT0
                 wrax    vol,0.0         ; write volume to register
         main:   ldax    input           ; read from input
